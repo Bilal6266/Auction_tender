310a745ee653e41bf7e6391be2addd0a4bdcf60c
Hamza Ramzan <52911062+Hamza708@users.noreply.github.com>
2023-04-25 16:42:32 +0500

    bn

--- AuctionTenderApi/auction_tender_startup/models.py
+++ AuctionTenderApi/auction_tender_startup/models.py
@@ -1,48 +1,48 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
-from django.db import models
-
-
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-    role = models.CharField(max_length=50)
+# # Create your models here.
+# from django.db import models
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='product_images')
-    description = models.TextField()
-    category = models.CharField(max_length=50)
+# class User(models.Model):
+#     user_id = models.AutoField(primary_key=True)
+#     name = models.CharField(max_length=255)
+#     email = models.EmailField(unique=True)
+#     password = models.CharField(max_length=255)
+#     role = models.CharField(max_length=50)
 
 
-class Review(models.Model):
-    review_id = models.AutoField(primary_key=True)
-    stars = models.IntegerField()
-    description = models.TextField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+# class Product(models.Model):
+#     name = models.CharField(max_length=255)
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     image = models.ImageField(upload_to='product_images')
+#     description = models.TextField()
+#     category = models.CharField(max_length=50)
 
 
-class Wishlist(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+# class Review(models.Model):
+#     review_id = models.AutoField(primary_key=True)
+#     stars = models.IntegerField()
+#     description = models.TextField()
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 
-class AddToCart(models.Model):
-    id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    added_date = models.DateField()
+# class Wishlist(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 
-class ProductHistory(models.Model):
-    id = models.AutoField(primary_key=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    action = models.CharField(max_length=50)
-    action_date = models.DateField()
-    description = models.TextField(blank=True, null=True)
+# class AddToCart(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     quantity = models.IntegerField()
+#     added_date = models.DateField()
+
+
+# class ProductHistory(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     action = models.CharField(max_length=50)
+#     action_date = models.DateField()
+#     description = models.TextField(blank=True, null=True)
